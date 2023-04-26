2179b7457dce0ca7b4a022395311c1b5ee5a80a9
Fercho7-9 <116390070+Fercho7-9@users.noreply.github.com>
2023-04-26 09:30:40 -0500

    Add files via upload

---
+++ Estructuras_Datos/Arreglo_Uni.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Arreglo_Uni {
+    public static void main(String[] args) {
+       /* int myArray [] = new int[5];
+        char miChar[] = {'a','b','c'};
+        String cars[] = {"Volvo","Bmv"};
+        }
+
+        */
+        int myarry []= new int[5];
+        Scanner teclado = new Scanner(System.in);
+        for (int i = 0; i <myarry.length ; i++) {
+            System.out.println("\n Dato ["+i+"]:");
+
+             }
+        }
+
+
+    }
+
